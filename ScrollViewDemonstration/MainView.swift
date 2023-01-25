c9cb564cd3a186a82da2e5cd2b37710a8b2a3ef7
phcelos <paulohcelos@gmail.com>
2023-01-25 17:40:34 -0300

    Create MainView.

---
+++ ScrollViewDemonstration/MainView.swift
@@ -0,0 +1,52 @@
+//
+//  MainView.swift
+//  ScrollViewDemonstration
+//
+//  Created by Paulo Henrique on 25/01/23.
+//
+
+import UIKit
+
+class MainView: UIView {
+    private let scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .green
+        
+        setupViewHierarchy()
+        setupConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViewHierarchy() {
+        addSubview(scrollView)
+        scrollView.addSubview(contentView)
+    }
+    
+    private func setupConstraints() {
+        scrollView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+    }
+}
--- ScrollViewDemonstration/ViewController.swift
+++ ScrollViewDemonstration/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let mainVew: UIView = {
+        let view = MainView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViewHierarchy()
+        setupConstraints()
     }
-
-
+    
+    private func setupViewHierarchy() {
+        view.addSubview(mainVew)
+    }
+    
+    private func setupConstraints() {
+        mainVew.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        mainVew.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        mainVew.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        mainVew.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
 }
 
